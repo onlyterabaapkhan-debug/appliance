6dbfb7dd3ee791b3af934e2d0b8e33ea0ef34fe5
punk <onlyterabaapkhan@gmail.com>
2026-07-06 12:01:13 +0500

    Simplify next.config.ts configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  typescript: { ignoreBuildErrors: true },
   reactStrictMode: false,
 };
-
 export default nextConfig;
